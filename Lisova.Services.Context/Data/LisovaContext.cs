76c87b2bd1ddece956a9d07e03d581cdfb912afc
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-14 20:31:46 +0300

    Added basic context to access data with future entities.

---
+++ Lisova.Services.Context/Data/LisovaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lisova.Services.Context.Data;
+
+public class LisovaContext : DbContext
+{
+    private const string ConnectionString = "Data Source=./Enterprise.db";
+    
+    public LisovaContext() { }
+
+    public LisovaContext(DbContextOptions<LisovaContext> contextOptions) : base(contextOptions) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(ConnectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
