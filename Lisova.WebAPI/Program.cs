6a1022d15f5b2b9bebddca8480bb6604ab60d506
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-15 20:21:12 +0300

    Added reading a file and connection string from the configuration file. Additionally, checking the presence of this data.

--- Lisova.WebAPI/Program.cs
+++ Lisova.WebAPI/Program.cs
@@ -1,10 +1,15 @@
 using Lisova.Services.Context.Data;
 using Lisova.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Get connection data stings, check if connection data is present 
+var sqliteDatabaseFile = builder.Configuration.GetValue<string?>("SQLiteDatabaseFile", null) ?? throw new Exception("The `SQLiteDatabaseFile` configuration setting is not set.");
+var connectionString = builder.Configuration.GetConnectionString("LisovaDatabase") ?? throw new Exception("The `LisovaDatabase` connection string is not set.");
+
 // Add services to the container.
-builder.Services.AddDbContext<LisovaContext>();
+builder.Services.AddDbContext<LisovaContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,9 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
