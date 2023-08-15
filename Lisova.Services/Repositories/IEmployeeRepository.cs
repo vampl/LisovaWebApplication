05134922ba5f791523ba53db74e301349c331304
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-15 16:11:42 +0300

    Added repository interface to define basic methods of storing and accessing data from context.

---
+++ Lisova.Services/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using Lisova.Services.Models;
+
+namespace Lisova.Services.Repositories;
+
+/// <summary>
+/// Represents a repository for storing a collection of objects of the <see cref="Employee"/> type.
+/// </summary>
+public interface IEmployeeRepository
+{
+    /// <summary>
+    /// Gets collection of all employees records in database.
+    /// </summary>
+    /// <returns>Returns collection of employees.</returns>
+    public ICollection<Employee> GetEmployees();
+
+    /// <summary>
+    /// Gets collection of employees in specified range of records in database.
+    /// </summary>
+    /// <param name="skip">The number of employee records to skip before adding an employee to the result collection.</param>
+    /// <param name="count">The number of employee records in the result collection.</param>
+    /// <returns></returns>
+    public ICollection<Employee> GetEmployeesRange(int skip, int count);
+    
+    /// <summary>
+    /// Get single employee record by its `employeeNo` in database.
+    /// </summary>
+    /// <param name="employeeNo">The unique identifier of an employee.</param>
+    /// <returns></returns>
+    public Employee GetEmployee(long employeeNo);
+}
