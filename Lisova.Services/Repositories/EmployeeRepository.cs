a324556aaf612a5db003b86541b9a081c3d94cf9
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-15 16:12:18 +0300

    Added basic implementation of IEmployeeRepository.

---
+++ Lisova.Services/Repositories/EmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Lisova.Services.Context.Data;
+using Lisova.Services.Models;
+
+namespace Lisova.Services.Repositories;
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    private readonly LisovaContext _lisovaContext;
+    
+    public EmployeeRepository(LisovaContext lisovaContext)
+    {
+        _lisovaContext = lisovaContext;
+    }
+    
+    public ICollection<Employee> GetEmployees()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<Employee> GetEmployeesRange(int skip, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employee GetEmployee(long employeeNo)
+    {
+        throw new NotImplementedException();
+    }
+}
