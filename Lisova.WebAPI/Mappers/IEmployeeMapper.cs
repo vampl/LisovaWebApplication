e5355c79d3a4490066843e1ee6b81ee6e1ead3f9
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-16 19:29:20 +0300

    Cleanup, remove unused methods, documentation comments.

--- Lisova.WebAPI/Mappers/IEmployeeMapper.cs
+++ Lisova.WebAPI/Mappers/IEmployeeMapper.cs
@@ -3,17 +3,22 @@ using Lisova.WebAPI.Models;
 
 namespace Lisova.WebAPI.Mappers;
 
+/// <summary>
+/// Represents an utility to map data from repository to request different data formats.
+/// </summary>
 public interface IEmployeeMapper
 {
+    /// <summary>
+    /// Map an employee object from repository employee model to full data representation.
+    /// </summary>
+    /// <param name="employee">The repository employee data to map into full employee model.</param>
+    /// <returns>Returns a full employee model with all required fields.</returns>
     public FullEmployee MapToFullEmployee(Employee employee);
 
-    public IList<FullEmployeePosition> MapToFullEmployeePositions(IList<EmployeePosition> employeePositions);
-    
-    public IList<FullEmployeeDepartment> MapToFullEmployeeDepartments(IList<EmployeeDepartment> employeeDepartments);
-    
+    /// <summary>
+    /// Map an employee object from repository employee model to abbreviated data representation.
+    /// </summary>
+    /// <param name="employee">The repository employee data to map into abbreviated employee model.</param>
+    /// <returns>Returns a abbreviated employee model with all required fields.</returns>
     public AbbreviatedEmployee MapToAbbreviatedEmployee(Employee employee);
-
-    public IList<AbbreviatedEmployeePosition> MapToAbbreviatedEmployeePositions(IList<EmployeePosition> employeePositions);
-
-    public IList<AbbreviatedEmployeeDepartment> MapToAbbreviatedEmployeeDepartments(IList<EmployeeDepartment> employeeDepartments);
 }
