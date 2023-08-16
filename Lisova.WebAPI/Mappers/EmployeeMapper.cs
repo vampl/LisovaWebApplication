10b41c9d00db9ca84a63272bff3cb7de849dbf9a
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-16 19:44:37 +0300

    Cleanup, documentation comments.

--- Lisova.WebAPI/Mappers/EmployeeMapper.cs
+++ Lisova.WebAPI/Mappers/EmployeeMapper.cs
@@ -3,8 +3,16 @@ using Lisova.WebAPI.Models;
 
 namespace Lisova.WebAPI.Mappers;
 
+/// <summary>
+/// An utility class implementing IEmployeeMapper to transform data.
+/// </summary>
 public class EmployeeMapper : IEmployeeMapper
 {
+    /// <summary>
+    /// Map an employee object from repository employee model to full data representation.
+    /// </summary>
+    /// <param name="employee">The repository employee data to map into full employee model.</param>
+    /// <returns>Returns a full employee model with all required fields.</returns>
     public FullEmployee MapToFullEmployee(Employee employee)
     {
         return new FullEmployee
@@ -19,17 +27,12 @@ public class EmployeeMapper : IEmployeeMapper
             EmployeeDepartments = MapToFullEmployeeDepartments(employee.EmployeeDepartments)
         };
     }
-
-    public IList<FullEmployeePosition> MapToFullEmployeePositions(IList<EmployeePosition> employeePositions)
-    {
-        return employeePositions.Select(MapToFullEmployeePosition).ToList();
-    }
-
-    public IList<FullEmployeeDepartment> MapToFullEmployeeDepartments(IList<EmployeeDepartment> employeeDepartments)
-    {
-        return employeeDepartments.Select(MapToFullEmployeeDepartment).ToList();
-    }
-
+    
+    /// <summary>
+    /// Map an employee object from repository employee model to abbreviated data representation.
+    /// </summary>
+    /// <param name="employee">The repository employee data to map into abbreviated employee model.</param>
+    /// <returns>Returns a abbreviated employee model with all required fields.</returns>
     public AbbreviatedEmployee MapToAbbreviatedEmployee(Employee employee)
     {
         return new AbbreviatedEmployee
@@ -39,17 +42,32 @@ public class EmployeeMapper : IEmployeeMapper
             EmployeeDepartments = MapToAbbreviatedEmployeeDepartments(employee.EmployeeDepartments)
         };
     }
-
-    public IList<AbbreviatedEmployeePosition> MapToAbbreviatedEmployeePositions(IList<EmployeePosition> employeePositions)
+    
+    /// <summary>
+    /// Maps a list of employee position objects from repository model to full data representation.
+    /// </summary>
+    /// <param name="employeePositions">The list of repository employee positions data to map into full employee position models</param>
+    /// <returns>Returns a list of all full employee position models with all required fields.</returns>
+    private static IList<FullEmployeePosition> MapToFullEmployeePositions(IList<EmployeePosition> employeePositions)
     {
-        return employeePositions.Select(MapToAbbreviatedEmployeePosition).ToList();
+        return employeePositions.Select(MapToFullEmployeePosition).ToList();
     }
 
-    public IList<AbbreviatedEmployeeDepartment> MapToAbbreviatedEmployeeDepartments(IList<EmployeeDepartment> employeeDepartments)
+    /// <summary>
+    /// Maps a list of employee departments objects from repository model to full data representation.
+    /// </summary>
+    /// <param name="employeeDepartments">The list of repository employee departments data to map into full employee department models</param>
+    /// <returns>Returns a list of all full employee department models with all required fields.</returns>
+    private static IList<FullEmployeeDepartment> MapToFullEmployeeDepartments(IList<EmployeeDepartment> employeeDepartments)
     {
-        return employeeDepartments.Select(MapToAbbreviatedEmployeeDepartment).ToList();
+        return employeeDepartments.Select(MapToFullEmployeeDepartment).ToList();
     }
     
+    /// <summary>
+    /// Maps a employee position object from repository model to full data representation.
+    /// </summary>
+    /// <param name="employeePosition">The repository employee position data to map into full employee position model</param>
+    /// <returns>Returns full employee position model with all required fields.</returns>
     private static FullEmployeePosition MapToFullEmployeePosition(EmployeePosition employeePosition)
     {
         return new FullEmployeePosition
@@ -63,6 +81,11 @@ public class EmployeeMapper : IEmployeeMapper
         };
     }
 
+    /// <summary>
+    /// Maps a employee department object from repository model to full data representation.
+    /// </summary>
+    /// <param name="employeeDepartment">The repository employee department data to map into full employee department model</param>
+    /// <returns>Returns full employee department model with all required fields.</returns>
     private static FullEmployeeDepartment MapToFullEmployeeDepartment(EmployeeDepartment employeeDepartment)
     {
         return new FullEmployeeDepartment
@@ -75,6 +98,31 @@ public class EmployeeMapper : IEmployeeMapper
         };
     }
     
+    /// <summary>
+    /// Maps a list of employee position objects from repository model to abbreviate data representation.
+    /// </summary>
+    /// <param name="employeePositions">The list of repository employee positions data to map into abbreviate employee position models</param>
+    /// <returns>Returns a list of all abbreviate employee position models with all required fields.</returns>
+    private static IList<AbbreviatedEmployeePosition> MapToAbbreviatedEmployeePositions(IList<EmployeePosition> employeePositions)
+    {
+        return employeePositions.Select(MapToAbbreviatedEmployeePosition).ToList();
+    }
+
+    /// <summary>
+    /// Maps a list of employee departments objects from repository model to abbreviate data representation.
+    /// </summary>
+    /// <param name="employeeDepartments">The list of repository employee departments data to map into abbreviate employee department models</param>
+    /// <returns>Returns a list of all abbreviate employee department models with all required fields.</returns>
+    private static IList<AbbreviatedEmployeeDepartment> MapToAbbreviatedEmployeeDepartments(IList<EmployeeDepartment> employeeDepartments)
+    {
+        return employeeDepartments.Select(MapToAbbreviatedEmployeeDepartment).ToList();
+    }
+    
+    /// <summary>
+    /// Maps a employee position object from repository model to abbreviate data representation.
+    /// </summary>
+    /// <param name="employeePosition">The repository employee position data to map into abbreviate employee position model</param>
+    /// <returns>Returns abbreviate employee position model with all required fields.</returns>
     private static AbbreviatedEmployeePosition MapToAbbreviatedEmployeePosition(EmployeePosition employeePosition)
     {
         return new AbbreviatedEmployeePosition
@@ -84,6 +132,11 @@ public class EmployeeMapper : IEmployeeMapper
         };
     }
 
+    /// <summary>
+    /// Maps a employee department object from repository model to abbreviate data representation.
+    /// </summary>
+    /// <param name="employeeDepartment">The repository employee department data to map into abbreviate employee department model</param>
+    /// <returns>Returns abbreviate employee department model with all required fields.</returns>
     private static AbbreviatedEmployeeDepartment MapToAbbreviatedEmployeeDepartment(EmployeeDepartment employeeDepartment)
     {
         return new AbbreviatedEmployeeDepartment
