ec05e152df649cc34c63aba8ba4789a45e7a64dd
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-16 16:49:25 +0300

    Added interface of employee mapper to define the necessary mappings.

---
+++ Lisova.WebAPI/Mappers/IEmployeeMapper.cs
@@ -0,0 +1,19 @@
+﻿using Lisova.Services.Models;
+using Lisova.WebAPI.Models;
+
+namespace Lisova.WebAPI.Mappers;
+
+public interface IEmployeeMapper
+{
+    public FullEmployee MapToFullEmployeeModel(Employee employee);
+
+    public FullEmployeePosition MapToFullEmployeePositionModel(EmployeePosition employeePosition);
+
+    public FullEmployeeDepartment MapToFullEmployeeDepartmentModel(EmployeeDepartment employeeDepartment);
+    
+    public FullEmployee MapToAbbreviatedEmployeeModel(Employee employee);
+
+    public FullEmployeePosition MapToAbbreviatedEmployeePositionModel(EmployeePosition employeePosition);
+
+    public FullEmployeeDepartment MapToAbbreviatedEmployeeDepartmentModel(EmployeeDepartment employeeDepartment);
+}
