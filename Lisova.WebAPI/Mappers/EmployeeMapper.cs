7c44e6cd80d43786218dd1d8a86df3b34607fcdc
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-08-16 16:50:01 +0300

    Added class to implement IEmployeeMapper, only basic structure.

---
+++ Lisova.WebAPI/Mappers/EmployeeMapper.cs
@@ -0,0 +1,37 @@
+﻿using Lisova.Services.Models;
+using Lisova.WebAPI.Models;
+
+namespace Lisova.WebAPI.Mappers;
+
+public class EmployeeMapper : IEmployeeMapper
+{
+    public FullEmployee MapToFullEmployeeModel(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FullEmployeePosition MapToFullEmployeePositionModel(EmployeePosition employeePosition)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FullEmployeeDepartment MapToFullEmployeeDepartmentModel(EmployeeDepartment employeeDepartment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FullEmployee MapToAbbreviatedEmployeeModel(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FullEmployeePosition MapToAbbreviatedEmployeePositionModel(EmployeePosition employeePosition)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FullEmployeeDepartment MapToAbbreviatedEmployeeDepartmentModel(EmployeeDepartment employeeDepartment)
+    {
+        throw new NotImplementedException();
+    }
+}
